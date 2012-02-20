8c8a6f1677b5667d59da3e2015b3e3c930c68ae8
Mikko <mikko@mfabrik.com>
2012-02-20 17:41:32 +0200

    Working on checkout form

--- collective/cartman/static/cartman-plone.js
+++ collective/cartman/static/cartman-plone.js
@@ -2,16 +2,75 @@
  * Plone bindings for Eric Cartman
  */
 
-/*global Cartman,CartmanUI*/
+/*global window,Cartman,CartmanUI,cartmanOptions*/
 
 (function($) {
 
     "use strict";
 
+    var cartman, ui;
+
+    /**
+     * If we detect PFG page and a certain field on it
+     *
+     * - Set this fields value to our JSON product payload
+     *
+     * - Print checout list on PFG form (non-editable)
+     */
+    function retrofitPloneFormGen() {
+        var field = $(cartmanOptions.productFieldSelector);
+
+        if(field.size() === 0) {
+            return;
+        }
+
+        // Fill in <hidden> input with data to be posted to Plone server
+        var productJSON = cartman.getContentsJSON();
+        field.val(productJSON);
+
+        // Render a <table> containing file product listing
+        // using Transparency
+        var data = ui.getCartTemplateData();
+
+        // Template directives
+        var directives = {
+
+            // Show empty cart warning
+            empty : function(elem) { if(data.count > 0) { elem.remove(); } },
+
+            // Show empty cart warning
+            filled : function(elem) { if(data.count <= 0) { elem.remove(); } },
+
+            // Nested directives for product lines
+            products : {
+                price : function() { return ui.formatPrice(this.price); },
+                total : function() { return ui.formatPrice(this.count*this.price); },
+                // Fill in image column only if image URL is available
+                "img@src" : function(elem) { if(this.img) { return this.img; } else { elem.remove(); } }
+            }
+        };
+
+        var listing = $("<div>");
+
+        var template = $(cartmanOptions.checkoutFormTemplateSelector);
+
+        if(template.size() == 0) {
+            throw new Error("Checkout form template missing:" + cartmanOptions.checkoutFormTemplateSelector);
+        }
+
+        // Explicitly used Transparency Template on jQuery node
+        listing.data("template", template);
+
+        // Render the template
+        listing.render(data, directives);
+
+        listing.insertAfter(field);
+    }
+
     $(document).ready(function() {
 
-        var cartman = new Cartman();
-        var ui = new CartmanUI({
+        cartman = new Cartman();
+        ui = new CartmanUI({
             cartman : cartman,
             selectors : {
                 addToCartAnimator : "input[type=number]"
@@ -21,9 +80,19 @@
         // Bootstrap UI
         ui.init();
 
+        // Integrate Plone checkout logic
+        // We have defined checkout URL in viewlet
+        // Javascript snippet.
+        // When checkout is pressed we go to this URL
+        ui.doCheckout = function() {
+            // cartmanOptions is populated by a viewlet
+            window.location = cartmanOptions.checkoutURL;
+        };
+
         // Load cart from localStorage,
         cartman.refreshStore();
 
+        retrofitPloneFormGen();
     });
 
 })(jQuery);
--- collective/cartman/viewlets.py
+++ collective/cartman/viewlets.py
@@ -1,7 +1,9 @@
 """
 
+    Cart viewlets.
 
 """
+import json
 
 from zope.interface import Interface
 from zope.component import getMultiAdapter, queryMultiAdapter
@@ -17,6 +19,68 @@ from plone.app.layout.viewlets.interfaces import IPortalTop
 from utils import has_mini_cart
 
 grok.templatedir("templates")
+
+# The generated HTML snippet going to <head>
+TEMPLATE = u"""
+<script type="text/javascript">
+    var %(yourJavascriptSettingsGlobal)s = %(json)s;
+</script>
+"""
+
+class JavascriptSettingsSnippet(grok.Viewlet):
+    """ Include dynamic Javascript code in <head>.
+
+    Include some code in <head> section which initializes
+    Javascript variables. Later this code can be used
+    by various scripts.
+
+    Useful for settings.
+    """
+
+    grok.context(Interface)
+
+    # This viewlet will be render()'ed in <head> section of Plone pages
+    grok.viewletmanager(IHtmlHead)
+
+    def getSettings(self):
+        """
+        @return: Python dictionary of settings
+        """
+
+        context = self.context.aq_inner
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+
+        # Create youroptions Javascript object and populate in these variables
+        return {
+            # Pass dynamically allocated site URL to the Javascripts (virtual host monster thing)
+            "staticMediaURL" : portal_state.portal_url() + "/++resource++collective.cartman",
+
+            # Some other example parameters
+            "portalURL" : portal_state.portal_url(),
+
+            # Where to move for checkout
+            "checkoutURL" : portal_state.portal_url() + "/checkout",
+
+            # jQuery selector to retrofit PFG with product data
+            "productFieldSelector" : "input[name=productdata]",
+
+            # jQuery selector for the template which renders
+            # PFG checkout form product <table>
+            "checkoutFormTemplateSelector" : "#checkout-form-template"
+        }
+
+
+    def render(self):
+        """
+        Render the settings as inline Javascript object in HTML <head>
+        """
+        settings = self.getSettings()
+        json_snippet = json.dumps(settings)
+
+        # Use Python string template facility to produce the code
+        html = TEMPLATE % { "yourJavascriptSettingsGlobal" : "cartmanOptions", "json" : json_snippet }
+
+        return html
 
 class MiniCart(grok.Viewlet):
     """ Cart summary line """
@@ -46,6 +110,13 @@ class CartTemplates(grok.Viewlet):
     grok.viewletmanager(IPortalFooter)
     grok.template("cart-templates")
 
+class FormTemplates(grok.Viewlet):
+    """
+    Client-side template sources for checkout form.
+    """
+    grok.context(Interface)
+    grok.viewletmanager(IPortalFooter)
+    grok.template("form-templates")
 
 class AddToCart(grok.Viewlet):
     """
